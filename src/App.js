d569c60cf57fbeeb236f1dd9d40af8026e2dadeb
scott <scottcooperaus1992@gmail.com>
2023-12-13 10:14:05 +1100

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+
+// always need a handle and a setter 
+function App(){
+
+    const [books, setBooks ] = useState("")
+    return <div>App</div>
+
+}
+
+export default App;
